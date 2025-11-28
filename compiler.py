9b7748acd5d026e19e756fbee94c324d306fc94b
Phil Hunt <philiphuntiv2@gmail.com>
2025-11-28 13:00:41 -0500

    Update compiler.py

--- compiler.py
+++ compiler.py
@@ -3,28 +3,43 @@ from philh_myftp_biz.web import download
 from philh_myftp_biz.array import List
 from philh_myftp_biz.json import Dict
 from philh_myftp_biz.pc import Path
-from typing import Any
 
+#
 tempfile = temp('mimetypes', 'json')
 
+#
 download(
     url = 'https://raw.githubusercontent.com/patrickmccallum/mimetype-io/refs/heads/master/src/mimeData.json',
     path = tempfile
 )
 
-raw: List[dict[str, Any]] = List(JSON(tempfile))
+#
+raw: List[dict] = List(JSON(tempfile))
 
+#
+override: Dict[str] = Dict(JSON(Path('override.json')))
+
+#
 compiled: Dict[str] = Dict(JSON(Path('compiled.json')))
+#
 compiled.save({})
 
+#
 for data in raw:
 
-    if 'name' in data:
+    if ('name' in data) and ('/' in data['name']):
 
-        if '/' in data['name']:
+        #
+        type = data['name'].split('/')[0]
 
-            type = data['name'].split('/')[0]
+        #
+        for ext in data['fileTypes']:
 
-            for ext in data['fileTypes']:
+            #
+            compiled[ext[1:]] = type
 
-                compiled[ext[1:]] = type
+#
+for ext, type in override.items():
+    
+    #
+    compiled[ext] = type
